029a769984eea867407f069c024d4c2292535a73
Yashi Pallav <puneetgupta5294@gmail.com>
2017-08-07 15:08:00 +0530

    project setup

---
+++ magicalGridAnimation/magicalGridAnimation/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  magicalGridAnimation
+//
+//  Created by Puneet on 07/08/17.
+//  Copyright © 2017 PuneetGupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
