d1f89295c843b27537169adb05f3de93a82952ae
Yashi Pallav <puneetgupta5294@gmail.com>
2017-08-07 16:51:29 +0530

    optimize for loops into dictionary

--- magicalGridAnimation/magicalGridAnimation/ViewController.swift
+++ magicalGridAnimation/magicalGridAnimation/ViewController.swift
@@ -9,14 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let numViewPerRow = 15
+    let numViewPerColumn = 30
+    var cells = [String: UIView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        let numViewPerRow = 15
-        let numViewPerColumn = 30
         let width = view.frame.width / CGFloat(numViewPerRow)
+       
         //let height = view.frame.height / CGFloat(numViewPerColumn)
         //print("w",width)
         //print("h",height)
@@ -30,12 +31,44 @@ class ViewController: UIViewController {
             cellView.layer.borderWidth = 0.5
             cellView.layer.borderColor = UIColor.black.cgColor
             view.addSubview(cellView)
+                
+                let key = "\(i)|\(j)"
+                cells[key] = cellView
+                
+                
             }
         }
         
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handlePan)))
         
         
     }
+    
+    func handlePan(gesture: UIPanGestureRecognizer) {
+        let location = gesture.location(in: view)
+        //print(location)
+        
+        let width = view.frame.width / CGFloat(numViewPerRow)
+        let i = Int(location.x / width)
+        let j = Int(location.y / width)
+        print(i,j)
+        
+        let key = "\(i)|\(j)"
+        let cellView = cells[key]
+        cellView?.backgroundColor = .white
+        
+//        var loopCount = 0
+//        for subview in view.subviews {
+//            if subview.frame.contains(location){
+//            
+//                subview.backgroundColor = .black
+//                //print("loopCount", loopCount)
+//                
+//            }
+//            loopCount += 1
+//        }
+    }
+    
     fileprivate func randomColor() -> UIColor {
         let red = CGFloat(drand48())
         let green = CGFloat(drand48())
